306282cfd971f30afc9e22120abe6d0648065216
Benjinho <benja.diaz.mno10@gmail.com>
2024-11-20 11:32:08 -0300

    Integracion de clase Bus

---
+++ Proyecto/src/main/java/org/example/Bus.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Bus {
+    String patente;
+    int capacidad;
+    int totalAsientos;
+    ArrayList<String> asientos = new ArrayList<>();
+
+    public Bus (){
+
+    }
+}
