db3263c6f5672f8c154b7da375dbc87a76b2580c
jolu1000 <joaquinarribel@gmail.com>
2024-11-23 23:45:33 -0300

    Arreglo de la subclase bus 2 pisos

--- Proyecto/src/main/java/Logica/Bus2Pisos.java
+++ Proyecto/src/main/java/Logica/Bus2Pisos.java
@@ -1,13 +1,11 @@
 package Logica;
 
 public class Bus2Pisos extends Bus {
-    private int asientosPrimerPiso;
-    private int asientosSegundoPiso;
 
-    public Bus2Pisos(String patente, int capacidad, int estandar, int semiCama, int cama, int premium, int asientosPrimerPiso, int asientosSegundoPiso) {
+    public Bus2Pisos(int capacidad, int estandar, int semiCama, int cama, int premium) {
         super(capacidad);
-        this.asientosPrimerPiso = asientosPrimerPiso;
-        this.asientosSegundoPiso = asientosSegundoPiso;
         inicializarAsientos(estandar, semiCama, cama, premium);
     }
+
 }
+
